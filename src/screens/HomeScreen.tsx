34cc305594f0ebd3b569f084f6c825ec69bdbbb6
kyle beavin <kyle.beavin@tcmcllc.com>
2021-10-18 12:35:42 -0400

    changing layout

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,62 @@
 import React from 'react';
+import { Card, Row, Col } from 'react-bootstrap';
 import AppTitle from '../components/layout/AppTitle';
 
 const HomeScreen = () => {
     return (
-        <>
-        <AppTitle title="Home" />
-      </>
+      <Row>
+        <Col>
+      <Card
+        style={{ maxHeight: '80vh', minHeight: '60vh', overflow: 'hidden', height: '100%' }}
+        className='m-3'
+      >
+        <Card.Header as="h5">
+          <Row>
+            <Col sm='10'>
+              Home
+            </Col>
+          </Row>
+        </Card.Header>
+        <Card.Body style={{ height: '100%' }}>
+
+        </Card.Body>
+      </Card>
+      </Col>
+      <Col>
+      <Card
+        style={{ maxHeight: '80vh', minHeight: '60vh', overflow: 'hidden', height: '100%' }}
+        className='m-3'
+      >
+        <Card.Header as="h5">
+          <Row>
+            <Col sm='10'>
+              Home
+            </Col>
+          </Row>
+        </Card.Header>
+        <Card.Body style={{ height: '100%' }}>
+          
+        </Card.Body>
+      </Card>
+      </Col>
+      <Col>
+      <Card
+        style={{ maxHeight: '80vh', minHeight: '60vh', overflow: 'hidden', height: '100%' }}
+        className='m-3'
+      >
+        <Card.Header as="h5">
+          <Row>
+            <Col sm='10'>
+              Home
+            </Col>
+          </Row>
+        </Card.Header>
+        <Card.Body style={{ height: '100%' }}>
+          
+        </Card.Body>
+      </Card>
+      </Col>
+    </Row>
     );
 }
 
--- src/screens/header/ReportsScreen.tsx
+++ src/screens/header/ReportsScreen.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 import AppTitle from '../../components/layout/AppTitle';
 
 const ReportsScreen = () => {
     return (
-        <>
-        <AppTitle title="Reports" />
-      </>
+      <>
+      <Card
+        style={{ maxHeight: '80vh', minHeight: '60vh', overflow: 'hidden', height: '100%' }}
+        className='m-3'
+      >
+        <Card.Header as="h5">
+          <Row>
+            <Col sm='10'>
+              Reports
+            </Col>
+          </Row>
+        </Card.Header>
+        <Card.Body style={{ height: '100%' }}>
+          <div
+            style={{ height: '100%', overflowY: 'auto' }}
+          >
+            {/* <Table
+              ref={dataTable}
+              id='table_id'
+              style={{ fontSize: '12px' }}
+              responsive
+              hover
+              size="sm"
+            >
+            </Table> */}
+          </div>
+        </Card.Body>
+      </Card>
+    </>
     );
 }
 
