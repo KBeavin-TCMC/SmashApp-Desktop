dbc35abbb79b1a1cdbd3d1e991f28a096d540d6b
kyle beavin <kyle.beavin@tcmcllc.com>
2022-01-05 10:29:21 -0500

    rm import

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'mapbox-gl/dist/mapbox-gl.css';
 // import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
